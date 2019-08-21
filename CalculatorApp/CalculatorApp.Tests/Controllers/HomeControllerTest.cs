3a1bdbb9051ff16193cba2ba987857639c0b96c9
infy_787981 <infy_787981@outlook.com>
2019-08-21 10:34:25 +0530

    Initial commit

---
+++ CalculatorApp/CalculatorApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp;
+using CalculatorApp.Controllers;
+using CalculatorApp.Models;
+
+namespace CalculatorApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        HomeController homeController;
+        CalculatorModels calculatorModels;
+        [TestInitialize]
+        public void CreateController()
+        {
+            homeController = new HomeController();
+            calculatorModels = new CalculatorModels();
+        }
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Act
+            var result = homeController.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Act
+            var result = homeController.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string command = "add";
+
+            //Act
+            ViewResult result = homeController.Index(calculatorModels, command) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string command = "sub";
+
+
+            //Act
+            ViewResult result = homeController.Index(calculatorModels, command) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string command = "mul";
+
+            //Act
+            ViewResult result = homeController.Index(calculatorModels, command) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string command = "div";
+
+            //Act
+            ViewResult result = homeController.Index(calculatorModels, command) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            CalculatorModels model = null;
+
+            //Act
+            ViewResult result = homeController.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CalculatorApp.Models;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/    
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CalculatorModels model, string command)
+        {
+            if (command == "add")
+            {
+                model.Result = model.A + model.B;
+            }
+            if (command == "sub")
+            {
+                model.Result = model.A - model.B;
+            }
+            if (command == "mul")
+            {
+                model.Result = model.A * model.B;
+            }
+            if (command == "div")
+            {
+                model.Result = model.A / model.B;
+            }
+            return View(model);
+        }
+    }
+}
